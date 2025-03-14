8d31e378302ece9e9277097801f49653d8060d8a
dylan <825880445@qq.com>
2025-03-14 10:13:34 +0800

    save

--- src/views/basic/scene.vue
+++ src/views/basic/scene.vue
@@ -1,93 +1,142 @@
 <script setup>
 import { onMounted } from 'vue'
 import * as THREE from 'three'
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+
 function main() {
+  const canvas = document.querySelector('#c')
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+  const gui = new GUI();
 
-	const canvas = document.querySelector( '#c' );
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+  const fov = 40
+  const aspect = 2 // the canvas default
+  const near = 0.1
+  const far = 1000
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+  camera.position.set(0, 50, 0)
+  camera.up.set(0, 0, 1)
+  camera.lookAt(0, 0, 0)
 
-	const fov = 40;
-	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 1000;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.set( 0, 50, 0 );
-	camera.up.set( 0, 0, 1 );
-	camera.lookAt( 0, 0, 0 );
+  const scene = new THREE.Scene()
 
-	const scene = new THREE.Scene();
+  {
+    const color = 0xffffff
+    const intensity = 500
+    const light = new THREE.PointLight(color, intensity)
+    scene.add(light)
+  }
 
-	{
+  const objects = []
 
-		const color = 0xFFFFFF;
-		const intensity = 500;
-		const light = new THREE.PointLight( color, intensity );
-		scene.add( light );
+  const radius = 1
+  const widthSegments = 6
+  const heightSegments = 6
+  const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
 
-	}
+  const solarSystem = new THREE.Object3D()
+  scene.add(solarSystem)
+  objects.push(solarSystem)
 
-	const objects = [];
+  const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 })
+  const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial)
+  sunMesh.scale.set(5, 5, 5)
+  solarSystem.add(sunMesh)
+  objects.push(sunMesh)
 
-	const radius = 1;
-	const widthSegments = 6;
-	const heightSegments = 6;
-	const sphereGeometry = new THREE.SphereGeometry(
-		radius, widthSegments, heightSegments );
+  const earthOrbit = new THREE.Object3D()
+  earthOrbit.position.x = 10
+  solarSystem.add(earthOrbit)
+  objects.push(earthOrbit)
 
-	const sunMaterial = new THREE.MeshPhongMaterial( { emissive: 0xFFFF00 } );
-	const sunMesh = new THREE.Mesh( sphereGeometry, sunMaterial );
-	sunMesh.scale.set( 5, 5, 5 );
-	scene.add( sunMesh );
-	objects.push( sunMesh );
+  const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233ff, emissive: 0x112244 })
+  const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial)
+  earthOrbit.add(earthMesh)
+  objects.push(earthMesh)
 
-	const earthMaterial = new THREE.MeshPhongMaterial( { color: 0x2233FF, emissive: 0x112244 } );
-	const earthMesh = new THREE.Mesh( sphereGeometry, earthMaterial );
-	earthMesh.position.x = 10;
-	scene.add( earthMesh );
-	objects.push( earthMesh );
+  const moonOrbit = new THREE.Object3D()
+  moonOrbit.position.x = 2
+  earthOrbit.add(moonOrbit)
 
-	function resizeRendererToDisplaySize( renderer ) {
+  const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 })
+  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial)
+  moonMesh.scale.set(0.5, 0.5, 0.5)
+  moonOrbit.add(moonMesh)
+  objects.push(moonMesh)
 
-		const canvas = renderer.domElement;
-		const width = canvas.clientWidth;
-		const height = canvas.clientHeight;
-		const needResize = canvas.width !== width || canvas.height !== height;
-		if ( needResize ) {
+  // Turns both axes and grid visible on/off
+  // GUI requires a property that returns a bool
+  // to decide to make a checkbox so we make a setter
+  // can getter for `visible` which we can tell GUI
+  // to look at.
+  class AxisGridHelper {
+    constructor(node, units = 10) {
+      const axes = new THREE.AxesHelper()
+      axes.material.depthTest = false
+      axes.renderOrder = 2 // after the grid
+      node.add(axes)
 
-			renderer.setSize( width, height, false );
+      const grid = new THREE.GridHelper(units, units)
+      grid.material.depthTest = false
+      grid.renderOrder = 1
+      node.add(grid)
 
-		}
+      this.grid = grid
+      this.axes = axes
+      this.visible = false
+    }
+    get visible() {
+      return this._visible
+    }
+    set visible(v) {
+      this._visible = v
+      this.grid.visible = v
+      this.axes.visible = v
+    }
+  }
 
-		return needResize;
+  function makeAxisGrid(node, label, units) {
+    const helper = new AxisGridHelper(node, units)
+    gui.add(helper, 'visible').name(label)
+  }
 
-	}
+  makeAxisGrid(solarSystem, 'solarSystem', 26)
+  makeAxisGrid(sunMesh, 'sunMesh')
+  makeAxisGrid(earthOrbit, 'earthOrbit')
+  makeAxisGrid(earthMesh, 'earthMesh')
+  makeAxisGrid(moonOrbit, 'moonOrbit')
+  makeAxisGrid(moonMesh, 'moonMesh')
 
-	function render( time ) {
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement
+    const width = canvas.clientWidth
+    const height = canvas.clientHeight
+    const needResize = canvas.width !== width || canvas.height !== height
+    if (needResize) {
+      renderer.setSize(width, height, false)
+    }
 
-		time *= 0.001;
+    return needResize
+  }
 
-		if ( resizeRendererToDisplaySize( renderer ) ) {
+  function render(time) {
+    time *= 0.001
 
-			const canvas = renderer.domElement;
-			camera.aspect = canvas.clientWidth / canvas.clientHeight;
-			camera.updateProjectionMatrix();
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement
+      camera.aspect = canvas.clientWidth / canvas.clientHeight
+      camera.updateProjectionMatrix()
+    }
 
-		}
+    objects.forEach((obj) => {
+      obj.rotation.y = time
+    })
 
-		objects.forEach( ( obj ) => {
+    renderer.render(scene, camera)
 
-			obj.rotation.y = time;
+    requestAnimationFrame(render)
+  }
 
-		} );
-
-		renderer.render( scene, camera );
-
-		requestAnimationFrame( render );
-
-	}
-
-	requestAnimationFrame( render );
-
+  requestAnimationFrame(render)
 }
 onMounted(() => {
   main()
